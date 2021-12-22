fdf79641e0eb42e315909f87866ef73d1678f357
Jan Bajena <bajena3@gmail.com>
2021-12-22 23:13:06 +0100

    Day 14 final

--- src/Day14.kt
+++ src/Day14.kt
@@ -14,9 +14,15 @@ fun main() {
 
     return result
   }
-
+  var cache = mutableMapOf<Pair<String, Int>, Map<Char, Long>>()
   fun compute(pair: String, rules: Map<String, Char>, maxLevel : Int = 10, current: Map<Char, Long> = mutableMapOf(), level: Int = 0) : Map<Char, Long> {
     var newMap = current.toMutableMap()
+
+    if (cache.getOrDefault(Pair(pair, level), null) != null) {
+      cache[Pair(pair, level)]!!.forEach { (letter, count) -> newMap[letter] = newMap.getOrDefault(letter, 0) + count }
+      return newMap
+    }
+
 
     val product = rules.getOrDefault(pair, null)
 
@@ -24,11 +30,18 @@ fun main() {
       newMap[pair.first()] = newMap.getOrDefault(pair.first(), 0) + 1
       newMap[pair.last()] = newMap.getOrDefault(pair.last(), 0) + 1
 
+//      cache[Pair(pair, level)] = newMap
+
       return newMap
     }
 
-    newMap = compute("${pair.first()}$product", rules, maxLevel, newMap, level + 1).toMutableMap()
-    newMap = compute("$product${pair.last()}", rules, maxLevel, newMap, level + 1).toMutableMap()
+    var resultLeft = compute("${pair.first()}$product", rules, maxLevel, newMap, level + 1).toMutableMap()
+    cache[Pair("${pair.first()}$product", level + 1)] = resultLeft
+    var resultRight = compute("$product${pair.last()}", rules, maxLevel, newMap, level + 1).toMutableMap()
+    cache[Pair("$product${pair.last()}", level + 1)] = resultRight
+
+    resultLeft.forEach { (letter, count) -> newMap[letter] = newMap.getOrDefault(letter, 0) + count }
+    resultRight.forEach { (letter, count) -> newMap[letter] = newMap.getOrDefault(letter, 0) + count }
     newMap[product] = newMap.getOrDefault(product, 0) - 1
 
     return newMap
@@ -54,7 +67,6 @@ fun main() {
     println(initial)
     for (i in 1..2) {
       result = step(result, rules)
-      println("After $i steps")
     }
 
     val occurences = result.groupingBy { it }.eachCount()
@@ -82,18 +94,15 @@ fun main() {
 
     initial.zipWithNext().forEach {
       var counts = compute("${it.first}${it.second}", rules, 40)
-      println("Pair:$it, counts:$counts")
       counts.forEach { (letter, count) -> result[letter] = result.getOrDefault(letter, 0) + count }
 
       result[it.second] = result.getOrDefault(it.second, 0) - 1
     }
     result[initial.last()] = result.getOrDefault(initial.last(), 0) + 1
 
-    println(result)
-    println(result.values.sum())
     println(result.values.maxOrNull()!! - result.values.minOrNull()!!)
   }
 
-//  part1()
+  part1()
   part2()
 }
