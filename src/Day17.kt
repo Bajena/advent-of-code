a17aacbf586a92680a5055cc12796a101fb13fd6
Jan Bajena <bajena3@gmail.com>
2021-12-28 22:14:36 +0100

    Day 17 part 2

--- src/Day17.kt
+++ src/Day17.kt
@@ -57,6 +57,32 @@ fun main() {
     return maxHeight
   }
 
+  fun calcCoords(vx: Int, minX: Int, maxX: Int, minY: Int, maxY: Int) : List<Pair<Int, Int>> {
+    var currentVx = vx
+    var steps = 0
+    var currentX = 0
+
+    while (currentX < minX && currentVx > 0) {
+      steps++
+      currentX += currentVx
+      if (currentVx > 0) currentVx--
+    }
+
+    if (currentX !in minX..maxX) return listOf()
+
+    var result = mutableListOf<Pair<Int, Int>>()
+    for (vy in -1000..1000) {
+      val maxStep = computeY(vy, currentX, currentVx, steps, minX, maxX, minY, maxY)
+
+      if (maxStep > 0) {
+        result.add(Pair(vx, vy))
+        println("vx:$vx, vy:$vy")
+      }
+    }
+
+    return result
+  }
+
   fun part1() {
     var minX = 20
     var maxX = 30
@@ -76,15 +102,34 @@ fun main() {
     for (vx in minVx..maxX) {
       val height = calcStepOfHit(vx, minX, maxX, minY, maxY)
       if (height > maxHeight) maxHeight = height
-//      println("Vx:$vx, steps: ${calcStepOfHit(vx, minX, maxX, minY, maxY)}")
     }
 
     println(maxHeight)
   }
 
   fun part2() {
+//    var minX = 20
+//    var maxX = 30
+//    var minY = -10
+//    var maxY = -5
+//
+//    var minVx = 6 // https://www.wolframalpha.com/input/?i=x2%2Bx-40%3D0
+
+    var minX = 25
+    var maxX = 67
+    var minY = -260
+    var maxY = -200
+
+    var minVx = 7 // https://www.wolframalpha.com/input/?i=x2%2Bx-50%3D0
+
+    var result = mutableListOf<Pair<Int, Int>>()
+    for (vx in minVx..maxX) {
+      result.addAll(calcCoords(vx, minX, maxX, minY, maxY))
+    }
+
+    println(result.count())
   }
 
-  part1()
-//  part2()
+//  part1()
+  part2()
 }
