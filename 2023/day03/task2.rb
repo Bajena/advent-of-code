602c7db25fde80db91f750b55c482b9d3452a771
Bajena <jan.bajena@productboard.com>
2023-12-06 22:31:59 +0100

    day03

---
+++ 2023/day03/task2.rb
@@ -0,0 +1,106 @@
+require 'debug'
+
+class FileReader
+  def initialize(file_path = "input.txt")
+    @file_path = file_path
+  end
+
+  def lines
+    File.readlines(@file_path)
+  end
+end
+
+class PartNumber
+  def initialize(start_x, end_x, y, number)
+    @start_x = start_x
+    @end_x = end_x
+    @y = y
+    @number = number
+  end
+
+  def add_digit(digit)
+    @number = (@number.to_s + digit.to_s).to_i
+    @end_x += 1
+  end
+
+  attr_accessor :start_x, :end_x, :y, :number
+end
+
+class SchemaSymbol
+  def initialize(x, y, symbol)
+    @x = x
+    @y = y
+    @symbol = symbol
+  end
+
+  def adjacent_to?(part_number)
+    @x.between?(part_number.start_x - 1, part_number.end_x + 1) && @y.between?(part_number.y - 1, part_number.y + 1)
+  end
+
+  attr_accessor :x, :y, :symbol
+end
+
+class Gear
+  def initialize(numbers)
+    raise "Invalid numbers" unless numbers.size == 2
+
+    @numbers = numbers
+  end
+
+  def ratio
+    @numbers.first * @numbers.last
+  end
+end
+
+class Task
+  def run
+    part_numbers = []
+    symbols = []
+
+    FileReader.new.lines.each.with_index do |line, y|
+      current_part_number = nil
+
+      line.split('').reject { |c| c == "\n" }.each.with_index do |char, x|
+        if number?(char)
+          if current_part_number.nil?
+            current_part_number = PartNumber.new(x, x, y, char.to_i)
+            part_numbers << current_part_number
+          else
+            current_part_number.add_digit(char.to_i)
+          end
+        elsif char == "."
+          current_part_number = nil
+        else
+          symbols << SchemaSymbol.new(x, y, char)
+          current_part_number = nil
+        end
+      end
+    end
+
+    valid_parts = part_numbers.select do |part_number|
+      symbols.any? do |symbol|
+        symbol.adjacent_to?(part_number)
+      end
+    end
+
+    stars = symbols.select { |symbol| symbol.symbol == "*" }
+    gears = []
+
+    stars.each do |star|
+      adjacent_numbers = []
+      valid_parts.each do |part|
+        adjacent_numbers << part.number if star.adjacent_to?(part)
+      end
+
+      gears << Gear.new(adjacent_numbers) if adjacent_numbers.size == 2
+    end
+
+    puts gears.map(&:ratio).sum
+  end
+
+  def number?(string)
+    true if Integer(string) rescue false
+  end
+end
+
+Task.new.run
